6c09bb28e9385a400934a15f9420a18da7875f56
MKashanButt <kashanbutt566@gmail.com>
2024-10-21 22:36:56 +0500

    updated variable types and popup form content

--- src/utils/CounterHelper.ts
+++ src/utils/CounterHelper.ts
@@ -1,13 +1,13 @@
 export function counter(id: string) {
     const count = document.getElementById(id) as HTMLElement;
-    const statesNumber = 5
-    const experienceNumber = 80
-    const patientNumber = 1100
-    let i = 0
+    const statesNumber = 5;
+    const experienceNumber = 80;
+    const patientNumber = 1100;
+    let i = 0;
 
     if (count) {
         if (id === 'statesNumber') {
-            let interval = setInterval(() => {
+            const interval = setInterval(() => {
                 count.textContent = i.toString();
                 i++;
                 if (i > statesNumber) {
@@ -15,7 +15,7 @@ export function counter(id: string) {
                 }
             }, 200);
         } else if (id === 'experienceNumber') {
-            let interval = setInterval(() => {
+            const interval = setInterval(() => {
                 count.textContent = i.toString();
                 i += 20;
                 if (i > experienceNumber) {
@@ -23,7 +23,7 @@ export function counter(id: string) {
                 }
             }, 200);
         } else if (id === 'patientNumber') {
-            let interval = setInterval(() => {
+            const interval = setInterval(() => {
                 count.textContent = i.toString();
                 i += 100;
                 if (i > patientNumber) {
