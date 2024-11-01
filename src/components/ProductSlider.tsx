b162c0d5ad1d49dc28c22addac624e2a90767ca0
MKashanButt <kashanbutt566@gmail.com>
2024-11-01 20:17:22 +0500

    made slider to take data as props

--- src/components/ProductSlider.tsx
+++ src/components/ProductSlider.tsx
@@ -17,7 +17,21 @@ import HipBraces from "@/assets/images/hip-braces.jpg"
 import TenseUnit from "@/assets/images/tense-unit.jpg"
 import LymphedemaPumpMassagers from "@/assets/images/lymphedema-pump-massagers.jpg"
 
-export default function ProductSlider({ heading }: { heading: string }) {
+interface Product {
+    id: number;
+    image: string;
+    title: string;
+    description: string;
+    link: string;
+}
+
+interface SliderProps {
+    heading: string;
+    data: Product[];
+}
+
+export default function ProductSlider({ heading, data }: SliderProps) {
+
     return (
         <section className={styles.products}>
             <h2>{heading}</h2>
@@ -40,45 +54,15 @@ export default function ProductSlider({ heading }: { heading: string }) {
                 autoplay
                 loop
             >
-                <SwiperSlide className={styles.item}>
-                    <a href="#contactUs">
-                        <Image src={Wheelchairs} alt="" />
-                        <h3>Wheelchairs</h3>
-                        <p>Discover our range of comfortable and durable wheelchairs, designed for ease of use and
-                            enhanced
-                            mobility. Choose the perfect model to fit your lifestyle and needs.</p>
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className={styles.item}>
-                    <a href="#contactUs">
-                        <Image src={CgmMonitors} alt="" />
-                        <h3>CGM Monitors</h3>
-                        <p>Stay on top of your health with our continuous glucose monitors, providing real-time
-                            blood
-                            sugar
-                            data for optimal diabetes management. Easy to use and highly accurate.</p>
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className={styles.item}>
-                    <a href="#contactUs">
-                        <Image src={KneeBraces} alt="" />
-                        <h3>Knee Braces</h3>
-                        <p>Support and stabilize your joints with our high-quality braces, designed for maximum
-                            comfort
-                            and
-                            effectiveness. Ideal for injury recovery and daily use.</p>
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className={styles.item}>
-                    <a href="#contactUs">
-                        <Image src={HipBraces} alt="" />
-                        <h3>Hip Braces</h3>
-                        <p>Support and stabilize your joints with our high-quality braces, designed for maximum
-                            comfort
-                            and
-                            effectiveness. Ideal for injury recovery and daily use.</p>
-                    </a>
-                </SwiperSlide>
+                {data.map((item) => (
+                    <SwiperSlide key={item.id} className={styles.item}>
+                        <a href={item.link}>
+                            <Image src={item.image} alt={item.title} />
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                        </a>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </section>
     )
