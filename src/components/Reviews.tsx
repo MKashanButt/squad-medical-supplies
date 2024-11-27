b28c117dc4ab2ed162d16f65375919b3a0dffa76
MKashanButt <kashanbutt566@gmail.com>
2024-11-27 20:06:06 +0500

    made review section dynamic and updated styling

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,35 +3,64 @@ import Image from "next/image"
 import quotes from "@/assets/images/quotes.png"
 import styles from "@/assets/css/components/Reviews.module.css"
 
+interface Review {
+    id: number,
+    name: string,
+    position: string,
+    description: string,
+}
+
 export default function Reviews() {
+    const Reviews: Review[] = [
+        {
+            'id': 1,
+            'name': 'Demo',
+            'position': 'Head',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam veritatis enim cum eaque consequatur error sunt deleniti sequi voluptas laboriosam excepturi placeat voluptatum, eos vitae quam velit reiciendis aut facere magni?',
+        },
+        {
+            'id': 2,
+            'name': 'Demo',
+            'position': 'Head',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam veritatis enim cum eaque consequatur error sunt deleniti sequi voluptas laboriosam excepturi placeat voluptatum, eos vitae quam velit reiciendis aut facere magni?',
+        }
+        ,
+        {
+            'id': 3,
+            'name': 'Demo',
+            'position': 'Head',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam veritatis enim cum eaque consequatur error sunt deleniti sequi voluptas laboriosam excepturi placeat voluptatum, eos vitae quam velit reiciendis aut facere magni?',
+        }
+        ,
+        {
+            'id': 4,
+            'name': 'Demo',
+            'position': 'Head',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam veritatis enim cum eaque consequatur error sunt deleniti sequi voluptas laboriosam excepturi placeat voluptatum, eos vitae quam velit reiciendis aut facere magni?',
+        }
+        ,
+        {
+            'id': 5,
+            'name': 'Demo',
+            'position': 'Head',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam veritatis enim cum eaque consequatur error sunt deleniti sequi voluptas laboriosam excepturi placeat voluptatum, eos vitae quam velit reiciendis aut facere magni?',
+        }
+    ]
+
     return (
         <section className={styles.reviews}>
             <h2>Latest Reviews</h2>
             <div className={styles.stage}>
-                <div className={styles.item}>
-                    <Image src={quotes} alt="quote-icon" className={styles.icon} />
-                    <Image src="https://placehold.co/50" alt="review-avatar" width="50" height="50" className={styles.avatar} />
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas magnam delectus eveniet architecto voluptatibus explicabo consectetur, quia sed quaerat! Doloribus.</p>
-                    <hr />
-                    <span>Lorem</span>
-                </div>
-                <div className={styles.item}>
-                    <Image src={quotes} alt="quote-icon" className={styles.icon} />
-                    <Image src="https://placehold.co/50" alt="review-avatar" width="50" height="50" className={styles.avatar} />
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas magnam delectus eveniet architecto voluptatibus explicabo consectetur, quia sed quaerat! Doloribus.</p>
-                    <hr />
-                    <span>Lorem</span>
-                </div>
-                <div className={styles.item}>
-                    <Image src={quotes} alt="quote-icon" className={styles.icon} />
-                    <Image src="https://placehold.co/50" alt="review-avatar" width="50" height="50" className={styles.avatar} />
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas magnam delectus eveniet architecto voluptatibus explicabo consectetur, quia sed quaerat! Doloribus.</p>
-                    <hr />
-                    <span>Lorem</span>
-                </div>
+                {Reviews.map(e => (
+                    <div className={styles.item}>
+                        <Image src={quotes} alt="quote-icon" className={styles.icon} />
+                        <span className={styles.avatar}>{e.name.charAt(0)}</span>
+                        <h3>{e.name}</h3>
+                        <p>{e.description}</p>
+                        <hr />
+                        <span>{e.position}</span>
+                    </div>
+                ))}
             </div>
         </section>
     )
