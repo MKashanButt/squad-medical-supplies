a2b60d00bf2a045e297b09288a62c5eef986c3f5
MKashanButt <kashanbutt566@gmail.com>
2024-12-17 19:47:10 +0500

    updated "how to buy" to "How to Request"

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -179,49 +179,49 @@ export default function About() {
             <section className={styles.howToBuy}>
                 <Image src={shopping} alt="shopping-icon" />
                 <div className={`${styles.content} container`}>
-                    <h2>How to buy?</h2>
+                    <h2>How to Request a Product at Zero Out-of-Pocket Cost</h2>
                     <div className={styles.stage}>
                         <div className={styles.item}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 512 512">
-                                <path d="M448 341.37V170.61A32 32 0 00432.11 143l-152-88.46a47.94 47.94 0 00-48.24 0L79.89 143A32 32 0 0064 170.61v170.76A32 32 0 0079.89 369l152 88.46a48 48 0 0048.24 0l152-88.46A32 32 0 00448 341.37z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
-                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M69 153.99l187 110 187-110M256 463.99v-200" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" width={40} height={40}>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                             </svg>
                             <div className={styles.content}>
-                                <h3>Free Delivery</h3>
-                                <p>Order over $120</p>
+                                <h3>Submit Your Product Request Online</h3>
                             </div>
                         </div>
                         <div className={styles.item}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 512 512">
-                                <rect x="32" y="80" width="448" height="256" rx="16" ry="16" transform="rotate(180 256 208)" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M64 384h384M96 432h320" />
-                                <circle cx="256" cy="208" r="80" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
-                                <path d="M480 160a80 80 0 01-80-80M32 160a80 80 0 0080-80M480 256a80 80 0 00-80 80M32 256a80 80 0 0180 80" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" width={40} height={40}>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                             </svg>
                             <div className={styles.content}>
-                                <h3>Get Refund</h3>
-                                <p>Within 30 Days Return</p>
+                                <h3>Get Product Confirmation Call</h3>
                             </div>
                         </div>
                         <div className={styles.item}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 512 512">
-                                <rect x="48" y="144" width="416" height="288" rx="48" ry="48" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-                                <path d="M411.36 144v-30A50 50 0 00352 64.9L88.64 109.85A50 50 0 0048 159v49" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-                                <path d="M368 320a32 32 0 1132-32 32 32 0 01-32 32z" />
+                            <svg width="40" height="40" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" transform="rotate(0 0 0)">
+                                <path d="M10.9193 10.419C10.9192 10.0048 10.5834 9.669 10.1692 9.66904C9.755 9.66908 9.41925 10.0049 9.41929 10.4191V15.5816C9.41925 15.9958 9.755 16.3317 10.1692 16.3317C10.5834 16.3317 10.9192 15.996 10.9193 15.5818L10.9195 13.7504H13.0807L13.0807 15.5817C13.0807 15.9959 13.4165 16.3317 13.8307 16.3317C14.2449 16.3317 14.5807 15.9959 14.5807 15.5817L14.5807 10.419C14.5807 10.0048 14.2449 9.66904 13.8307 9.66904C13.4164 9.66904 13.0807 10.0048 13.0807 10.419V12.2504H10.9195L10.9193 10.419Z" fill="#ffffff" />
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M13.35 3.70391C12.55 3.10391 11.45 3.10391 10.65 3.70391L4.15 8.57891C3.58344 9.00383 3.25 9.6707 3.25 10.3789V18.5003C3.25 19.743 4.25736 20.7503 5.5 20.7503H18.5C19.7426 20.7503 20.75 19.743 20.75 18.5003V10.3789C20.75 9.6707 20.4166 9.00383 19.85 8.57891L13.35 3.70391ZM11.55 4.90391C11.8167 4.70391 12.1833 4.70391 12.45 4.90391L18.95 9.77891C19.1389 9.92055 19.25 10.1428 19.25 10.3789V18.5003C19.25 18.9145 18.9142 19.2503 18.5 19.2503H5.5C5.08579 19.2503 4.75 18.9145 4.75 18.5003V10.3789C4.75 10.1428 4.86115 9.92055 5.05 9.77891L11.55 4.90391Z" fill="#ffffff" />
                             </svg>
                             <div className={styles.content}>
-                                <h3>Safe Payment</h3>
-                                <p>100% Secure Payments</p>
+                                <h3>Doctor’s Approval for Medical Necessity</h3>
                             </div>
                         </div>
                         <div className={styles.item}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 512 512">
-                                <path d="M83 384c-13-33-35-93.37-35-128C48 141.12 149.33 48 256 48s208 93.12 208 208c0 34.63-23 97-35 128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
-                                <path d="M108.39 270.13l-13.69 8h0c-30.23 17.7-31.7 72.41-3.38 122.2s75.87 75.81 106.1 58.12h0l13.69-8a16.16 16.16 0 005.78-21.87L130 276a15.74 15.74 0 00-21.61-5.87zM403.61 270.13l13.69 8h0c30.23 17.69 31.74 72.4 3.38 122.19s-75.87 75.81-106.1 58.12h0l-13.69-8a16.16 16.16 0 01-5.78-21.87L382 276a15.74 15.74 0 0121.61-5.87z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32" />
+                            <svg width="40" height="40" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" transform="rotate(0 0 0)">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.24756 6C7.83756 6 7.49756 6.34 7.49756 6.75V8.39L5.99756 9.07V6.75C5.99756 5.51 7.00756 4.5 8.24756 4.5H8.99756V4.25C8.99756 3.01 10.0076 2 11.2476 2H12.7476C13.9876 2 14.9976 3.01 14.9976 4.25V4.5H15.7476C16.9876 4.5 17.9976 5.51 17.9976 6.75V9.07L16.4976 8.39V6.75C16.4976 6.34 16.1576 6 15.7476 6H8.24756ZM11.2476 3.5C10.8376 3.5 10.4976 3.84 10.4976 4.25V4.5H13.4976V4.25C13.4976 3.84 13.1576 3.5 12.7476 3.5H11.2476Z" fill="#ffffff" />
+                                <path d="M5.3776 17.6999C5.7776 17.7199 6.1676 17.8799 6.4676 18.1099C6.6776 18.2699 6.9276 18.4299 7.2076 18.5799C7.1976 18.5599 7.1901 18.5399 7.1826 18.5199C7.1751 18.4999 7.1676 18.4799 7.1576 18.4599L5.5876 13.0699C5.4776 12.7099 5.6676 12.3199 6.0176 12.1699L11.7076 9.73992C11.8976 9.65992 12.1076 9.65992 12.2976 9.73992L17.9876 12.1699C18.3376 12.3199 18.5176 12.6999 18.4176 13.0699L16.8476 18.4599C16.8476 18.4599 16.8176 18.5399 16.7976 18.5799C17.0776 18.4299 17.3276 18.2699 17.5376 18.1099C17.8376 17.8799 18.2276 17.7299 18.6276 17.6999L19.8476 13.4799C20.1676 12.3899 19.6176 11.2299 18.5676 10.7799L12.8776 8.34992C12.3176 8.10992 11.6776 8.10992 11.1076 8.34992L5.4176 10.7799C4.3676 11.2299 3.8176 12.3799 4.1376 13.4799L5.3576 17.6999H5.3776Z" fill="#ffffff" />
+                                <path d="M21.5577 20.32L18.8277 19.06C18.5377 18.93 18.1977 18.99 17.9777 19.22C17.9677 19.23 16.7077 20.49 15.2577 20.49C15.0677 20.49 14.8977 20.46 14.7277 20.43C13.5177 20.18 12.5577 19.22 12.5477 19.21C12.4077 19.07 12.2177 18.98 12.0077 18.98C11.8077 18.98 11.6177 19.06 11.4677 19.2C11.4577 19.21 10.4977 20.17 9.28769 20.42C9.22916 20.4265 9.17062 20.4351 9.11175 20.4438C8.98959 20.4618 8.86588 20.48 8.73769 20.48C7.28769 20.48 6.02769 19.22 6.01769 19.21C5.79769 18.98 5.45769 18.92 5.16769 19.05L2.43769 20.31C2.05769 20.48 1.89769 20.93 2.06769 21.3C2.23769 21.68 2.68769 21.84 3.06769 21.67L5.34769 20.62C5.96769 21.13 7.23769 21.98 8.73769 21.98C10.1377 21.98 11.3377 21.23 11.9977 20.72C12.6577 21.23 13.8577 21.98 15.2577 21.98C16.7577 21.98 18.0277 21.12 18.6477 20.62L20.9277 21.67C21.2977 21.84 21.7477 21.67 21.9277 21.3C22.0977 20.92 21.9277 20.48 21.5577 20.31V20.32Z" fill="#ffffff" />
                             </svg>
                             <div className={styles.content}>
-                                <h3>Safe Payment</h3>
-                                <p>100% Secure Payments</p>
+                                <h3>Fast and Secure Product Shipment</h3>
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" width={40} height={40}>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                            </svg>
+                            <div className={styles.content}>
+                                <h3>Insurance Claim Submission</h3>
                             </div>
                         </div>
                     </div>
