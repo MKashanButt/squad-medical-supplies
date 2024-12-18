52a3bc96249359645321948f909dbe6bc2644d86
MKashanButt <kashanbutt566@gmail.com>
2024-12-18 19:56:28 +0500

    adding troll name on copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
                     </div>
                 </div>
             </footer>
-            <p className={styles.copyright}> Copyright Company &#169; Squad Medical Supplies. All Rights Reserved 2020 - {new Date().getFullYear()}</p>
+            <p className={styles.copyright}> Copyright Company &#169; Squad Medical Supplies. All Rights Reserved 2020 - {new Date().getFullYear()} & Content By Ahmed Ayat (Chief Medical Assistant)</p>
         </>
     )
 }
