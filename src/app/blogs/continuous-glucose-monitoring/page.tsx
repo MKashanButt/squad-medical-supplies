b304e9a4846f73d389ac482d98fbd6d34c6343be
MKashanButt <kashanbutt566@gmail.com>
2024-12-19 21:41:51 +0500

    added blog and hero image

---
+++ src/app/blogs/continuous-glucose-monitoring/page.tsx
@@ -0,0 +1,51 @@
+import styles from '@/assets/css/SingleBlog.module.css';
+import Image from 'next/image';
+
+import hero from "@/assets/images/cgm-hero.jpg"
+
+export default function ContinuousGlucoseMonitoring() {
+    return (
+        <main>
+            <section className={styles.hero}>
+                <Image src={hero} alt='' />
+                <div className={styles.overlay}></div>
+                <h1>The Advantages of Continuous Glucose Monitoring: What Medicare Will Cover in 2025</h1>
+            </section>
+            <div className={styles.body}>
+                <p><span className={styles.firstLetter}>C</span>ontinuous glucose monitors (CGMs) are now a crucial component of the toolset needed to properly manage diabetes. Medicare has increased its coverage for CGMs in response to the rising prevalence of diabetes in the US, giving patients more access. Patients may more easily purchase and utilize CGMs in 2025 because to these regulatory improvements, which also make it simpler for them to keep an eye on their blood sugar levels in real time. This important technology improves overall health outcomes in addition to diabetes treatment. More patients may now use CGMs to better manage their conditions because to increased accessibility.</p>
+                <div className={styles.stage}>
+                    <div className={styles.item}>
+                        <h2>Continuous Glucose Monitors (CGMs): What Are They?</h2>
+                        <p>A tiny, wearable gadget called a continuous glucose monitor (CGM) continually measures blood sugar levels and provides real-time information. CGMs provide continuous monitoring and notify users of blood sugar trends or swings, in contrast to conventional fingerstick testing. Because of this, they are especially helpful for those with Type 1 or Type 2 diabetes, improving control in general.</p>
+                        <p>BThe three main parts of a CGM are a sensor that is placed beneath the skin to detect the amount of glucose in the interstitial fluid, a transmitter that transmits the data from the sensor, and a receiver or smartphone app that shows the glucose readings in a way that is easy for users to comprehend.</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h2>Continuous Glucose Monitor Advantages</h2>
+                        <p>Users can check their blood sugar levels around-the-clock with the use of continuous glucose monitors (CGMs), which give real-time glucose measurements. People with diabetes may make educated decisions regarding their food, exercise, and medication thanks to this ongoing data. Additionally, CGMs provide a more convenient monitoring strategy by eliminating the need for regular fingerstick tests.</p>
+                        <p>By giving steady glucose level insights and lowering the risks of both hyperglycemia and hypoglycemia, CGMs aid in improving glycemic management. By providing comfort, alerts for elevated or decreased blood sugar levels enhance people's quality of life. Furthermore, a lot of CGMs make it possible to share data with medical professionals, which guarantees improved assistance and more efficient diabetes care.</p>
+                    </div>
+                </div>
+                <p>For those with diabetes, Medicare's 2025 expansion of coverage for Continuous Glucose Monitors (CGMs) is quite beneficial. Patients must have diabetes and need regular blood sugar checks, have a doctor attest to the device's medical necessity, be taking insulin, or have a history of hypoglycemic episodes in order to be eligible for CGM coverage under Medicare. With these upgrades, a greater number of qualified people may now get the technology they need to better manage their blood sugar levels. This guarantees that more diabetics will be able to securely and effectively monitor and control their health.</p>
+                <p>A wider range of CGM devices, including those with cutting-edge capabilities, are now covered by the new Medicare coverage, giving patients additional choices. These gadgets can now have features like predictive warnings and smartphone connection, which make it easier for users to monitor their blood sugar levels in real time. This increased coverage guarantees that people with diabetes have access to the newest and most efficient technology in addition to the resources they need to effectively control their blood sugar levels. With additional choices, patients may choose the CGM model that best suits their requirements and way of life, giving them more authority over their health.</p>
+                <p>CGMs are covered by Medicare Part B as durable medical equipment (DME), and after paying their Part B deductible, patients are usually liable for 20% of the Medicare-approved amount. The remaining expenses may be covered by a number of secondary insurance policies, greatly lowering policyholders' out-of-pocket payments. Medicare also pays for the materials and accessories, such sensors and transmitters, needed for the efficient use of CGMs. This all-inclusive coverage makes CGM devices more accessible and inexpensive for people with diabetes by guaranteeing that patients have everything they need to use them correctly. Medicare continues to offer vital assistance for the treatment and control of diabetes with these enhancements.</p>
+                <h2>How to Use Medicare to Get a CGM</h2>
+                <p>The first step is to speak with your physician about how to fully benefit from Medicare's enhanced coverage for Continuous Glucose Monitors (CGMs). Talk about your needs for managing your diabetes and consider utilizing a CGM during your visit. Request a prescription from your physician attesting to the medical necessity of a CGM, if applicable. This is an important step since Medicare requires a doctor's paperwork in order to be covered. The approval process will go more smoothly and quickly if your healthcare provider complies with Medicare's eligibility requirements. To effectively use this crucial diabetes care tool, your doctor's assistance will be essential.</p>
+                <p>Selecting a Medicare-approved provider who can help you with the approval and delivery procedure is crucial next. Working with a reputable provider such as Squad Medical Supplies guarantees that you will receive knowledgeable advice and support about Medicare processes. They will guide you through the documentation, making sure that all conditions are satisfied and that the approval procedure is completed quickly. There will be fewer delays or issues if you work with a supplier who is knowledgeable with the Medicare system. Additionally, a trustworthy source will assist you in choosing the best CGM device for your requirements and tastes.</p>
+                <p>The next step is to submit the required paperwork after you've picked a supplier and spoken with your doctor. This includes any supporting medical data that could be needed, as well as your doctor's prescription attesting to the necessity of a CGM. To ensure everything is in order for Medicare to approve it, the provider will help you collect and submit the necessary documentation. Depending on your health plan and coverage, you will receive your CGM when the paperwork has been completed and authorized, frequently at no cost to you. The item will be sent to your home by your provider, who will arrange for its delivery.</p>
+                <div className={styles.stage}>
+                    <div className={styles.item}>
+                        <h2>Getting the Most Out of Your CGM</h2>
+                        <p>Set alerts for high or low glucose readings to start getting the most out of your CGM. Notifications that you may customize will help you stay informed and respond quickly. Furthermore, monitor glucose patterns over time to pinpoint triggers like diet or activity and modify your way of life for improved regulation.</p>
+                        <p>To maximize the effectiveness of your diabetes treatment strategy, share your CGM data with your medical team. Making the required therapy modifications is made possible by routinely going over data with your physician. Maintain your gadget consistently by swapping out sensors and transmitters as necessary. This keeps the CGM operating at its peak efficiency and guarantees the accuracy of your readings.</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h2>Frequently Asked Questions Regarding Medicare CGM Coverage</h2>
+                        <p>Smartphones are not necessary for all continuous glucose monitoring (CGMs). Users are free to select the manner that best suits their needs, since some versions provide standalone receivers in addition to smartphone connectivity. Regardless of smartphone usage, this adaptability guarantees that CGMs may satisfy a range of tastes and requirements.</p>
+                        <p>Medicare covers the basic model that is necessary for controlling your diabetes if you are thinking about upgrading your CGM. Upgrades might cost more unless your doctor says they are medically required. Contact your supplier to discuss repair or replacement alternatives if your CGM is damaged. Depending on certain conditions, Medicare may pay for certain expenses.</p>
+                    </div>
+                </div>
+                <p>Because they provide unparalleled ease, precision, and peace of mind, continuous glucose monitors, or CGMs, are completely changing the way diabetes is managed. More people will have access to this vital technology to enhance their health and quality of life after Medicare's coverage is extended in 2025. We at Squad Medical Supplies streamline the procedure by helping you get your Medicare-covered CGM. Our staff is prepared to help you choose the best gadget so you may take advantage of continuous monitoring and better control your diabetes. To find out more and begin your journey toward better diabetes treatment, get in touch with us right now!</p>
+            </div>
+        </main>
+    )
+}
