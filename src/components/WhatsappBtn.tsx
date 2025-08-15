8fde3b706e1421da239b717f7ca1f19f1a44cffa
MKashanButt <164230790+MKashanButt@users.noreply.github.com>
2025-08-15 20:08:18 +0000

    add btn styling and icon image

--- src/components/WhatsappBtn.tsx
+++ src/components/WhatsappBtn.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
+import styles from "@/assets/css/components/WhatsappButton.module.css";
+import whatsapp from "@/assets/images/whatsapp.png";
+import Image from "next/image";
 
 export default function WhatsappButton(){
     return(
-        <Link href="https://wa.me/14044422019" target="__blank" className="absolute bottom-5 left-5">
-            <button className="flex p-2 bg-green-700 flex items-center justify-center">
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000"
-                preserveAspectRatio="xMidYMid meet" className="w=5 h-5">
-                    <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                    fill="#000000" stroke="none">
-                    </g>
-                </svg>
+        <Link href="https://wa.me/14706670121" target="__blank">
+            <button className={styles.whatsappBtn}>
+                <Image src={whatsapp} alt="Whatsapp Image" className={styles.whatsappIcon} />
             </button>
         </Link>
     )
